1021002579fc606059037f77cf75918253e93f00
dim5x <dim5x@yahoo.com>
2024-06-03 19:27:26 +0300

    + js

---
+++ static/js/table_main.js
@@ -0,0 +1,40 @@
+fetch('/get_columns')
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error('Network response was not ok.');
+    })
+    .then(data => {
+        // Создание массива объектов столбцов на основе полученных данных
+        let serverColumns = data;
+        console.log('serverColumns', serverColumns);
+        // Создание столбцов в таблице Tabulator на основе serverColumns
+        let columns = serverColumns.map(column => {
+            return {
+                title: column.title,
+                // accessor: function (data) {
+                //     return data[column.field];
+                // },
+                field: column.field,
+                editor: column.editor
+                // Другие параметры столбца, если необходимо
+            };
+
+        });
+
+        console.log('columns', columns);
+// Создание таблицы
+        let table = new Tabulator("#table_main", {
+            ajaxURL: "/get_data_for_main_table",
+            ajaxConfig: "GET",
+            columns: columns,
+            layout: "fitData",
+
+            ajaxResponse: function (url, params, response) {
+                return response;
+            },
+        });
+    })
+
+
---
+++ static/js/table_players.js
@@ -0,0 +1,87 @@
+// Создание таблицы
+var table = new Tabulator("#example-table", {
+    // height: 205,
+    layout: "fitColumns",
+    columns: [
+        {title: "ID", field: "id", editor: false},
+        {title: "FIO", field: "fio", editor: "input"},
+        {title: "player_ID", field: "player_id", editor: "number"},
+    ],
+    // autoColumns: true,
+    ajaxURL: "/get_data_for_table_players",
+    ajaxConfig: "GET",
+    ajaxResponse: function (url, params, response) {
+        return response;
+    },
+});
+
+// Обновление данных
+document.getElementById('update-button').addEventListener('click', function () {
+    var updatedData = table.getData();
+    $.ajax({
+        url: 'http://127.0.0.1:5000/update',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify(updatedData),
+        success: function (response) {
+            alert('Data updated successfully!');
+        }
+    });
+});
+
+// Функция для обновления данных в таблице через AJAX и перерисовки
+function updateTableFromAjax() {
+    fetch('/get_data_for_table_players')
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Network response was not ok.');
+        })
+        .then(data => {
+            console.log('Обработка успешного ответа ', data); // Обработка успешного ответа от сервера
+            table.setData(data); // Установка новых данных из AJAX-ответа
+            table.redraw(true); // Обновление таблицы
+
+        })
+        .catch(error => {
+            console.error('Fetch error:', error); // Обработка ошибок
+        });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('player-form').addEventListener('submit', function (event) {
+        event.preventDefault(); // Предотвращаем отправку формы по умолчанию
+
+        let playerFIO = document.getElementById('player-fio').value;
+        let playerName = document.getElementById('player-name').value;
+        console.log(playerFIO, playerName);
+        let postData = {
+            playerFIO: playerFIO,
+            playerName: playerName
+        };
+
+        // Отправка данных на сервер с использованием метода POST
+        fetch('/update_table_players', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(postData)
+        })
+            .then(response => {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error('Network response was not ok.');
+            })
+            .then(data => {
+                console.log(data); // Обработка успешного ответа от сервера
+
+                updateTableFromAjax();
+            })
+            .catch(error => {
+                console.error('Fetch error:', error); // Обработка ошибок
+            });
+    });
+});
