bd49a2a157e43e9c572d8ea4183497cbe1e178d9
dim5x <dim5x@yahoo.com>
2024-06-19 07:33:25 +0300

    fix Assigned expression type string[] is not assignable to type string. - debug string

--- source/static/js/check_packets.js
+++ source/static/js/check_packets.js
@@ -1,6 +1,4 @@
 $(document).ready(function () {
-
-
     // Добавление нового поля ввода при нажатии на кнопку
     let inputCount = 1;
     $('#add-input').click(function () {
@@ -12,27 +10,6 @@ $(document).ready(function () {
             '</div>'
         );
     });
-
-    // function pack(data) {
-    //     console.log('data', typeof(data['data']), data['data']);
-    //     // Получаем список ссылок из переданных данных
-    //     var linkList = data['data'].split(','); // Разбиваем список ссылок по запятым
-    //
-    //     // Находим div, в который мы хотим поместить список ссылок
-    //     var divElement = document.getElementById('serverResponse'); // Замените 'yourDivId' на ID вашего div
-    //
-    //     // Очищаем содержимое div перед добавлением новых элементов
-    //     divElement.innerHTML = '';
-    //     divElement.innerHTML = linkList;
-    //     // Для каждой ссылки создаем элемент <a> и добавляем его в div
-    //     // linkList.forEach(function (link) {
-    //     //     // var linkElement = document.createElement('a');
-    //     //     // linkElement.href = link.trim(); // Убираем лишние пробелы
-    //     //     // linkElement.textContent = link.trim(); // Устанавливаем текст ссылки
-    //     //     divElement.appendChild(link); // Добавляем ссылку в div
-    //     //     divElement.appendChild(document.createElement('br')); // Добавляем перенос строки
-    //     // });
-    // }
 
     // Обработка отправки формы
     $('#game-form').submit(function (event) {
@@ -60,16 +37,12 @@ $(document).ready(function () {
             })
             .then(data => {
                 console.log(data); // Обработка успешного ответа от сервера
-                d = document.getElementById('serverResponse');
-                d.innerHTML =''
-                d.innerHTML = data['data'].split('\n');
-                // pack(data);
-                // updateTableFromAjax();
+                let d = document.getElementById('serverResponse');
+                d.innerHTML = data['data'].split('\n').join('<br>');
             })
             .catch(error => {
                 console.error('Fetch error:', error); // Обработка ошибок
             });
         // Здесь можно выполнить дополнительные действия с данными (например, отправка на сервер)
     });
-
 });
